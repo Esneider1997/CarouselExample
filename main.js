71e9f1961bb060b19b2f7d9e6a7ea4400bae4961
Esneider1997 <esneidermanuelbuelvas1997@gmail.com>
2021-07-02 16:36:18 -0500

    Agregado metodo para carrusel

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-const buttonPrev  = document.getElementById('button-prev');
-const buttonNext  = document.getElementById('button-next');
-const track  = document.getElementById('track'); /* Contenedor Imagenes */
-const slicklist  = document.getElementById('slick-list'); /* Contenedor de todo el Carousel */
-const slick = document.querySelectorAll('.slick') /* Clase de las 15 imagenes */
+// const buttonPrev  = document.getElementById('button-prev');
+// const buttonNext  = document.getElementById('button-next');
+// const track  = document.getElementById('track'); /* Contenedor Imagenes */
+// const slicklist  = document.getElementById('slick-list'); /* Contenedor de todo el Carousel */
+// const slick = document.querySelectorAll('.slick') /* Clase de las 15 imagenes */
 
-const slickWidth = slick[0].offsetWidth;
+// const slickWidth = slick[0].offsetWidth;
 
-buttonPrev.onclick = () => move(1);
-buttonNext.onclick = () => move(2);
+// buttonPrev.onclick = () => move(1);
+// buttonNext.o
+// nclick = () => move(2);
 
-function Move(value){
+// function Move(value){
+//     const trackWidth = track.offsetWidth;
+//     const listWidth = slickList.offsetWidth;
+
+// track.style.left == ""? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+
+//     if (leftPosition < (trackWidth - listWidth) && value == 2 ) {
+//         track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+//     }else if(leftPosition > 0 && value == 1 ){
+//         track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+//     }
+// }
+
+
+function App() {}
+
+window.onload = function (event) {
+    var app = new App();
+    window.app = app;
+};
+
+App.prototype.processingButton = function(event) {
+    const btn = event.currentTarget;
+    const slickList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const slick = track.querySelectorAll('.slick');
+
+    const slickWidth = slick[0].offsetWidth;
+    
     const trackWidth = track.offsetWidth;
     const listWidth = slickList.offsetWidth;
 
-track.style.left == ""? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+    track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
 
-    if (leftPosition < (trackWidth - listWidth) && value == 2 ) {
-        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
-    }else if(leftPosition > 0 && value == 1 ){
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition,slickWidth,track) : nextAction(leftPosition,trackWidth,listWidth,slickWidth,track)
+}
+
+let prevAction = (leftPosition,slickWidth,track) => {
+    if(leftPosition > 0) {
+        console.log("entro 2")
         track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
     }
 }
 
-
-
+let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
+    if(leftPosition < (trackWidth - listWidth)) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }
+}
 
 
 
