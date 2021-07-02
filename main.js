9607d0969aca14f777e77cc08ac9e3648965f8a3
Esneider1997 <esneidermanuelbuelvas1997@gmail.com>
2021-07-02 15:30:25 -0500

    svg modificado

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ const buttonPrev  = document.getElementById('button-prev');
 const buttonNext  = document.getElementById('button-next');
 const track  = document.getElementById('track'); /* Contenedor Imagenes */
 const slicklist  = document.getElementById('slick-list'); /* Contenedor de todo el Carousel */
-
 const slick = document.querySelectorAll('.slick') /* Clase de las 15 imagenes */
 
+const slickWidth = slick[0].offsetWidth;
 
+buttonPrev.onclick = () => move(1);
+buttonNext.onclick = () => move(2);
 
+function Move(value){
+    const trackWidth = track.offsetWidth;
+    const listWidth = slickList.offsetWidth;
 
+track.style.left == ""? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
 
+    if (leftPosition < (trackWidth - listWidth) && value == 2 ) {
+        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+    }else if(leftPosition > 0 && value == 1 ){
+        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+    }
+}
 
 
 
